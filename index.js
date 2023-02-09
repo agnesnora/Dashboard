7fc647a2450632faa265fc6d560e9599ea38fda1
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2023-02-09 11:48:39 +0100

    fetching

--- index.js
+++ index.js
@@ -23,5 +23,14 @@ fetch(
   .then((data) => {
     console.log(data);
     document.body.style.backgroundImage = `url(${data.urls.full})`;
-    document.getElementById("author").textContent = data.user.name;
+    document.getElementById("author").textContent = `By:${data.user.name}`;
+  })
+  .catch((err) => {
+    console.log(err);
+    document.body.style.background = `url("https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080")`;
   });
+
+fetch(`https://api.coingecko.com/api/v3/coins/vaiot`)
+  .then((res) => res.json())
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
